5c24f776fab32b6dcbd1bb2a479dac209362d9a6
Hoa <dqhoa06@gmail.com>
2026-05-30 03:52:13 +0700

    initial commit

---
+++ src/main/java/com/mycompany/call_center_waiting_line_system/Call_Center_Waiting_Line_System.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.call_center_waiting_line_system;
+
+/**
+ *
+ * @author dqhoa
+ */
+public class Call_Center_Waiting_Line_System {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
